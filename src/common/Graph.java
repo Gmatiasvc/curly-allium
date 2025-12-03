39eb4d684d53bfc5a4ab3096f76ca3f373adc6d6
Gerardo Venegas <gmatiasvc@hotmail.com>
2025-12-02 20:03:43 -0500

    Implement methods for adding, checking, and removing vertices and edges in the graph

--- src/common/Graph.java
+++ src/common/Graph.java
@@ -12,6 +12,56 @@ public class Graph<T> {
 		adjacencyList.get(adjacencyList.size() - 1).add(vertex);
 	}
 
+	public void addEdge(T from, T to) {
+		for (int i = 0; i < adjacencyList.size(); i++) {
+			LinkedList<T> list = adjacencyList.get(i);
+			if (list.get(0).equals(from)) {
+				list.add(to);
+				return;
+			}
+		}
+	}
 
+	public boolean containsVertex(T vertex) {
+		for (int i = 0; i < adjacencyList.size(); i++) {
+			if (adjacencyList.get(i).get(0).equals(vertex)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
+	public boolean containsEdge(T from, T to) {
+		for (int i = 0; i < adjacencyList.size(); i++) {
+			LinkedList<T> list = adjacencyList.get(i);
+			if (list.get(0).equals(from)) {
+				for (int j = 1; j < list.size(); j++) {
+					if (list.get(j).equals(to)) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	public boolean removeVertex(T vertex) {
+		for (int i = 0; i < adjacencyList.size(); i++) {
+			if (adjacencyList.get(i).get(0).equals(vertex)) {
+				adjacencyList.remove(adjacencyList.get(i));
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean removeEdge(T from, T to) {
+		for (int i = 0; i < adjacencyList.size(); i++) {
+			LinkedList<T> list = adjacencyList.get(i);
+			if (list.get(0).equals(from)) {
+				return list.remove(to);
+			}
+		}
+		return false;
+	}
 }
