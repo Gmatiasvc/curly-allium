8345c3c9247fa916a764be6d03de65b9d73e19b4
Gerardo Venegas <gmatiasvc@hotmail.com>
2025-12-19 12:31:59 -0500

    started implementing request handling in the server and client side

---
+++ src/client/RequestHandler.java
@@ -0,0 +1,8 @@
+package client;
+
+public class RequestHandler {
+	public String processRequest(String request) {
+
+		return "Echo: " + request;
+	}
+}
--- src/server/RequestHandler.java
+++ src/server/RequestHandler.java
@@ -1,8 +1,32 @@
 package server;
 
-public class RequestHandler {
-	public String processRequest(String request) {
+import java.util.ArrayList;
+import java.util.Arrays;
 
-		return "Echo: " + request;
+public class RequestHandler {
+
+	private ArrayList<String> parseRequest(String request) {
+		return new ArrayList<>(Arrays.asList(request.trim().split("&")));
+	}
+	
+	public String processRequest(String request) {
+		ArrayList<String> params = parseRequest(request);
+
+		switch (params.get(0)) {
+			case "000" -> {
+				return "Pong";
+                }
+
+			case "001" -> {
+				return "001&"+params.get(1);
+			}
+
+			case "069" ->{
+				return "069&Nice";
+			}
+			default -> throw new AssertionError();
+		}
+
+
 	}
 }
